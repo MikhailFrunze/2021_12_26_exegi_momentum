10b98568af974fee3f691e8ae810a0b0a5a1c5b4
MikhailFrunze <96693796+MikhailFrunze@users.noreply.github.com>
2022-02-15 20:33:04 +0100

    Create 2022_02_10_substring.java

---
+++ Java homework/2022_02_10_substring.java
@@ -0,0 +1,10 @@
+package de.telran;
+
+public class Main {
+
+    public static void substring(String str, int from, int to) {
+        for (int i = from; i < to; i++) {
+            System.out.print(str.charAt(i));
+        }
+    }
+}
