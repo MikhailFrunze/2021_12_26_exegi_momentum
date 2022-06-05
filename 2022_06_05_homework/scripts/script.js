b28cd83cbef78ce025caec3331075cbd01f13c48
MikhailFrunze <mihailfrunze1@gmail.com>
2022-06-05 14:41:02 +0200

    added homework

---
+++ 2022_06_05_homework/scripts/script.js
@@ -0,0 +1,12 @@
+console.log(100/4);
+console.log((8/2)+5-(-3/2));
+console.log("Lorem ipsum dolor sit amet");
+console.log(`Lorem ipsum dolor sit amet`);
+let number = 8;
+number = 15;
+console.log(number);
+const sum = 8 * 8;
+console.log(sum);
+const name = "Mihail";
+const lastName  = "Frunze";
+console.log(name, lastName);
